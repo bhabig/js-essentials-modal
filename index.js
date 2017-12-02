e40cc55dd31164e7471716edce30aa6fc68b7706
Bob Habig <rfhab2@gmail.com>
2017-12-02 01:32:24 -0500

    added more javascript, edited classes and ids in html, added css selector

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 (function() {
   // select button
-  button = document.querySelector('.modal-button');
-
+  let buttonOne = document.querySelector('#skill-check');
+  let buttonTwo = document.querySelector('#modal-check');
   //create event listener for click on button
-  button.addEventListener('click', (e) => {
+  buttonOne.addEventListener('click', (e) => {
     e.preventDefault();
     mathPuzzle();
-  })
+  });
+  buttonTwo.addEventListener('click', (e) => {
+    e.preventDefault();
+    fireModal();
+  });
 }())
 
 function mathPuzzle() {
@@ -15,18 +19,25 @@ function mathPuzzle() {
   const equationForPrompt = `(${num1} / 2) * (${num2} / 2) - 15`;
   const equationForSolution = (num1 / 2) * (num2 / 2) - 15;
   let solution = parseInt(prompt(`Solve this equation to enter: ${equationForPrompt}`));
-  let div = document.querySelector('.math-unlock')
 
   if (solution === equationForSolution) {
-    fireModal();
+    fireMath();
   } else {
     alert("This site won't be getting any more fun for you -__-");
   }
 }
 
-function fireModal() {
+function fireMath() {
   let pTag = document.querySelector('p');
   let name = prompt('...and what is your name?');
+  let div = document.querySelector('#post-math');
+  let button = document.querySelector('#pre-math');
+  button.classList.add('math-unlock');
+  button.classList.remove('modal-button');
   div.classList.remove('math-unlock');
   pTag.innerHTML += `<br><br>${name[0].toUpperCase()}${name.slice(1, name.legth)}, you GENIUS! Come on in`;
+}
+
+function fireModal() {
+  
 }
